1d2be173116891a7f656b33c3a3154845df56547
101010 <95350110+Frost101010@users.noreply.github.com>
2022-12-04 15:48:40 +0100

    Update AocDay4b).py

--- Day4/AocDay4b).py
+++ Day4/AocDay4b).py
@@ -7,7 +7,6 @@ for x in f:
     numbers = x.split(",")
     first = numbers[0].split("-")
     second = numbers[1].split("-")
-    a = list(range(int(first[0]),int(first[1])))
     if not isCovered(list(range(int(first[0]),(int(first[1]) + 1))), list(range(int(second[0]),(int(second[1]) + 1)))):
         sum += 1
 print(sum)
